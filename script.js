b688dfc031f7de29f029c86c246c308d292b0d75
ts-oh <tsohlove@gmail.com>
2021-10-30 14:28:38 +0900

    added an array to display books

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [
+	'Dune',
+	'Animal Farm',
+	'Lord of The Flies',
+	'Brave New World',
+	'Of Mice and Men',
+	'Lays of Ancient Rome',
+];
+
+function Book() {
+	//the consturctor
+	myLibrary.forEach((book) => console.log(book));
+}
+
+function addBookToLibrary() {
+	// add books
+}
