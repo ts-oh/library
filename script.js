670bdd65002a61190fa31a700c512959ccedb9c9
ts-oh <tsohlove@gmail.com>
2021-11-02 16:35:31 +0900

    created add button and pop-up form

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let myLibrary = [
-	'Dune',
-	'Animal Farm',
-	'Lord of The Flies',
-	'Brave New World',
-	'Of Mice and Men',
-	'Lays of Ancient Rome',
-];
+const bookDisplay = document.querySelector('#book-item');
+const openFormBtn = document.querySelector('#open-form');
+openFormBtn.addEventListener('click', openForm);
+const closeBtn = document.querySelector('.btn-close');
+closeBtn.addEventListener('click', closeForm);
+
+let myLibrary = [];
 
 function Book() {
-	//the consturctor
-	myLibrary.forEach((book) => console.log(book));
+	// the constructor
+	myLibrary.forEach((book) => {
+		const bookCard = document.createElement('h1');
+		bookCard.textContent = book;
+		bookDisplay.appendChild(bookCard);
+	});
 }
 
-function addBookToLibrary() {
+// An object constructor
+function addBookToLibrary(title, author, pages) {
 	// add books
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.info = function () {
+		return `${title} by ${author}, ${pages} pages`;
+	};
+}
+
+Book();
+
+function openForm() {
+	document.getElementById('book-form').style.display = 'block';
+}
+
+function closeForm() {
+	document.getElementById('book-form').style.display = 'none';
 }
