57560d21fd21bb3f7560f7f6d7a6efee25284826
ts-oh <tsohlove@gmail.com>
2021-11-04 12:40:00 +0900

    read data from form field

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const bookDisplay = document.querySelector('#book-item');
+
+// button to open pop-up form
 const openFormBtn = document.querySelector('#open-form');
 openFormBtn.addEventListener('click', openForm);
+
+// closing button for pop-up form
 const closeBtn = document.querySelector('.btn-close');
 closeBtn.addEventListener('click', closeForm);
+
+// accessing the values in the add book form
+const bookTitle = document.forms['form-container']['book-title'];
+
+let bookAuthor = document.forms['form-container']['book-author'].value;
+
+let bookPages = document.forms['form-container']['book-pages'].value;
+
+// add button to add new books from the pop-up form
+const addBook = document.querySelector('.btn-add');
+addBook.addEventListener('click', addBookToLibrary);
+
+const addBtn = document.querySelector('.btn-add');
+
+addBtn.addEventListener('click', function (e) {
+	e.preventDefault();
+	const bookTitleValue = bookTitle.value;
+	console.log(bookTitleValue);
+});
 
 let myLibrary = [];
 
@@ -29,9 +52,9 @@ function addBookToLibrary(title, author, pages) {
 Book();
 
 function openForm() {
-	document.getElementById('book-form').style.display = 'block';
+	document.querySelector('#book-popup').style.display = 'block';
 }
 
 function closeForm() {
-	document.getElementById('book-form').style.display = 'none';
+	document.querySelector('#book-popup').style.display = 'none';
 }
