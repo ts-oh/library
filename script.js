d6237e205c9227617404e05ef6c21d925fdb0283
ts-oh <tsohlove@gmail.com>
2021-12-04 14:43:48 +0700

    different structure, need to add read status

--- script.js
+++ script.js
@@ -1,104 +1,69 @@
-// book container div
-const bookContainer = document.querySelector('#card-container');
+let myLibrary = [];
 
-// button to open pop-up form
-const openFormBtn = document.querySelector('#open-form');
-openFormBtn.addEventListener('click', openForm);
+const openModalBtn = document.querySelector('#open-modal-btn');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const addBookBtn = document.querySelector('#add-book-btn');
+const title = document.querySelector('#title-input');
+const author = document.querySelector('#author-input');
+const pages = document.querySelector('#pages-input');
+const read = document.querySelector('input#readCheck[value]');
+const libraryContainer = document.querySelector('.library-container');
+//const deleteBookBtn = document.querySelectorAll('.delete-book-btn');
 
-// closing button for pop-up form
-const closeBtn = document.querySelector('.btn-close');
-closeBtn.addEventListener('click', closeForm);
+// deleteBookBtn.forEach((book) =>
+// 	book.addEventListener('click', function () {
+// 		console.log('button is clicked');
+// 	})
+// );
 
-// accessing the values in the add book form
-const bookTitle = document.forms['form-container']['book-title'];
-
-const bookAuthor = document.forms['form-container']['book-author'];
-
-const bookPages = document.forms['form-container']['book-pages'];
-
-const bookRead = document.forms['form-container']['readCheckBox'];
-
-// add button to add new books from the pop-up form
-const addBook = document.querySelector('.btn-add');
-addBook.addEventListener('click', createBook);
-
-const addBtn = document.querySelector('.btn-add');
-
-// button to delete each book
-const delBtn = document.querySelector('.btn-delete');
-
-function isRead(readValue) {
-	if (readValue === 'true') {
-		return 'Read';
-	} else if (readValue === 'false') {
-		return 'Not Read';
-	}
-}
-
-addBtn.addEventListener('click', function (e) {
-	e.preventDefault();
-	const titleVal = bookTitle.value;
-	const authorVal = bookAuthor.value;
-	const pagesVal = bookPages.value;
-	const readVal = bookRead.value;
-
-	const readBool = isRead(readVal);
-
-	const newBook = new createBook(titleVal, authorVal, pagesVal, readBool);
-	console.log(newBook);
-
-	myLibrary.push(newBook);
-
-	Book();
+openModalBtn.addEventListener('click', function () {
+	document.querySelector('#overlay').style.display = 'block';
 });
 
-let myLibrary = [];
-console.log(myLibrary);
+closeModalBtn.addEventListener('click', function () {
+	document.querySelector('#overlay').style.display = 'none';
+});
 
-function Book() {
-	const bookAddedTitle = myLibrary[myLibrary.length - 1].title;
-	const bookAddedAuthor = myLibrary[myLibrary.length - 1].author;
-	const bookAddedPages = myLibrary[myLibrary.length - 1].pages;
-	const bookAddedRead = myLibrary[myLibrary.length - 1].read;
+addBookBtn.addEventListener('click', function () {
+	let newBook = new Book(title.value, author.value, pages.value, read.value);
 
-	const bookCardTitle = document.createElement('h2');
-	const bookCardAuthor = document.createElement('p');
-	const bookCardPages = document.createElement('p');
-	const bookCardRead = document.createElement('p');
-	const bookDelete = document.createElement('button');
+	myLibrary.push(newBook);
+	console.log(myLibrary);
+	clearDisplay();
+	renderDisplay();
+});
 
-	bookDelete.classList.add('btn-delete');
-
-	bookCardTitle.textContent = bookAddedTitle;
-	bookCardAuthor.textContent = bookAddedAuthor;
-	bookCardPages.textContent = bookAddedPages;
-	bookCardRead.textContent = bookAddedRead;
-	bookDelete.textContent = 'Delete';
-
-	bookContainer.appendChild(bookCardTitle);
-	bookContainer.appendChild(bookCardAuthor);
-	bookContainer.appendChild(bookCardPages);
-	bookContainer.appendChild(bookCardRead);
-	bookContainer.appendChild(bookDelete);
-
-	delBtn.addEventListener('click', function (e) {
-		e.preventDefault();
-		console.log('this is delete btn');
-	});
-}
-
-// An object constructor
-function createBook(title, author, pages, read) {
+function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
 }
 
-function openForm() {
-	document.querySelector('#book-popup').style.display = 'block';
+// render read string from my library
+function renderDisplay() {
+	libraryContainer.innerHTML = '';
+	for (let i = 0; i < myLibrary.length; i++) {
+		libraryContainer.innerHTML += `<div class="book-container" data-index="${i}">
+				<p>Title: ${myLibrary[i].title}</p>
+				<p>Author: ${myLibrary[i].author}</p>
+				<p>Pages: ${myLibrary[i].pages}</p>
+				<button class="read-btn">
+					${myLibrary[i].read}
+				</button>
+				<button class="delete-book-btn" onclick="deleteBook(${i})">
+					Remove ✖️
+				</button>
+			</div>`;
+	}
 }
 
-function closeForm() {
-	document.querySelector('#book-popup').style.display = 'none';
+function clearDisplay() {
+	libraryContainer.innerHTML = '';
+}
+
+function deleteBook(index) {
+	myLibrary.splice(index, 1);
+	clearDisplay();
+	renderDisplay();
 }
