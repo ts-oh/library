64042a872630763557f93eba841587c7196e2d32
ts-oh <oh.timmms@gmail.com>
2022-01-22 14:08:42 +0700

    Revert "modified Book constructor to class"
    This reverts commit 1a3d95ad9f43716acb6f59f6f192cc1033c22aa0.

--- script.js
+++ script.js
@@ -60,13 +60,11 @@ function createNewBook() {
 	}
 }
 
-class Book {
-	constructor (title, author, pages, read) {
+function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
-	}
 }
 
 function trimBookObj() {
