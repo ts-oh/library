a7cbeee2279ee9a69e3c9b691ca4dce41d710fe0
ts-oh <oh.timmms@gmail.com>
2022-02-24 14:42:28 +0700

    replaced innerHTML and minor fix with html and css

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let myLibrary = [
 	{
-		title: 'Add your book Title',
-		author: 'The name of the Author',
-		pages: '777',
-		read: 'Yes',
+		title: 'Your Book Title',
+		author: 'Íte Marthe',
+		pages: '231',
+		read: 'no',
+	},
+	{
+		title: 'Cool Book Title',
+		author: 'Nir Helena',
+		pages: '137',
+		read: 'yes',
+	},
+	{
+		title: 'Awesome Book Title',
+		author: 'Adam Azriel',
+		pages: '341',
+		read: 'no',
+	},
+	{
+		title: 'Nice Book Title',
+		author: 'Òscar José Ángel',
+		pages: '507',
+		read: 'yes',
 	},
 ];
 
@@ -19,9 +37,7 @@ const libraryContainer = document.querySelector('.library-container');
 const error = document.querySelector('#error');
 
 openModalBtn.addEventListener('click', openModal);
-
 closeModalBtn.addEventListener('click', closeModal);
-
 addBookBtn.addEventListener('click', addNewBookToLibrary);
 
 function addNewBookToLibrary() {
@@ -37,22 +53,19 @@ function openModal() {
 
 function closeModal() {
 	document.querySelector('#overlay').style.display = 'none';
-	error.innerHTML = '';
-}
-
-function readValueCheck() {
-	if (read.checked === true) {
-		return (read.value = 'Yes');
-	} else if (notRead.checked === true) {
-		return (read.value = 'No');
-	}
+	error.textContent = '';
 }
 
 function createNewBook() {
 	if (title.value === '' && author.value === '' && pages.value === '') {
 		return (error.textContent = 'Please enter your book input ⚠️');
 	} else {
-		let newBook = new Book(title.value, author.value, pages.value, read.value);
+		let newBook = new Book(
+			title.value,
+			author.value,
+			pages.value,
+			read.value.toLowerCase()
+		);
 		myLibrary.push(newBook);
 		trimBookObj();
 		closeModal();
@@ -61,11 +74,19 @@ function createNewBook() {
 }
 
 class Book {
-	constructor (title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+}
+
+function readValueCheck() {
+	if (read.checked === true) {
+		return (read.value = 'yes');
+	} else if (notRead.checked === true) {
+		return (read.value = 'no');
 	}
 }
 
@@ -78,35 +99,78 @@ function trimBookObj() {
 }
 
 function renderDisplay() {
-	libraryContainer.innerHTML = '';
+	libraryContainer.textContent = '';
 	for (let i = 0; i < myLibrary.length; i++) {
-		libraryContainer.innerHTML += `<div class="book-container" data-index="${i}">
-				<p><b>Title:</b> ${myLibrary[i].title}</p>
-				<p><b>Author:</b> ${myLibrary[i].author}</p>
-				<p><b>Pages:</b> ${myLibrary[i].pages}</p>
-				<p><b>Read?:</b> ${myLibrary[i].read}</p> <button id="read-status-btn" onclick="readStatusChange(${i})">Read? 🙋🏻 or 🙅🏻‍♂️</button>
-				<button id="delete-book-btn" onclick="deleteBook(${i})">
-					Remove 📤
-				</button>
-			</div>`;
+		const bookContainer = document.createElement('div');
+		bookContainer.setAttribute('class', 'book-container');
+		bookContainer.setAttribute('data-index', `${i}`);
+		libraryContainer.append(bookContainer);
+
+		const bookTitle = document.createElement('p');
+		bookTitle.textContent = `Title: ${myLibrary[i].title}`;
+		bookContainer.append(bookTitle);
+
+		const bookAuthor = document.createElement('p');
+		bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
+		bookContainer.append(bookAuthor);
+
+		const bookPages = document.createElement('p');
+		bookPages.textContent = `Pages: ${myLibrary[i].pages}`;
+		bookContainer.append(bookPages);
+
+		const bookRead = document.createElement('p');
+		bookRead.textContent = `Read?: ${myLibrary[i].read}`;
+		bookContainer.append(bookRead);
+
+		const readBtn = document.createElement('button');
+		readBtn.setAttribute('class', 'read-status-btn');
+		readBtn.textContent = `Read? 🙋🏻/🙅🏻‍♂️`;
+		bookContainer.append(readBtn);
+
+		const deleteBtn = document.createElement('button');
+		deleteBtn.setAttribute('class', 'delete-book-btn');
+		deleteBtn.textContent = `Remove 💥`;
+		bookContainer.append(deleteBtn);
+	}
+
+	const bookReadBtn = document.querySelectorAll('.read-status-btn');
+
+	const bookReadBtnArr = [...bookReadBtn].forEach((btn) => {
+		btn.addEventListener('click', readCheck);
+	});
+
+	function readCheck(e) {
+		const indexNum = e.target.parentElement.dataset.index;
+		readStatusChange(indexNum);
+	}
+
+	const bookDeleteBtn = document.querySelectorAll('.delete-book-btn');
+
+	const bookDeleteBtnArr = [...bookDeleteBtn].forEach((btn) => {
+		btn.addEventListener('click', deleteCheck);
+	});
+
+	function deleteCheck(e) {
+		const indexNum = e.target.parentElement.dataset.index;
+		deleteBook(indexNum);
 	}
 }
 
 function clearDisplay() {
-	libraryContainer.innerHTML = '';
+	libraryContainer.textContent = '';
 }
 
-function deleteBook(index) {
-	myLibrary.splice(index, 1);
+function deleteBook(i) {
+	myLibrary.splice(0, 1);
 	clearDisplay();
 	renderDisplay();
 }
 
-function readStatusChange(index) {
-	if (myLibrary[index].read === 'Yes') {
-		myLibrary[index].read = 'No';
-	} else if (myLibrary[index].read === 'No') {
-		myLibrary[index].read = 'Yes';
+function readStatusChange(i) {
+	if (myLibrary[i].read === 'yes') {
+		myLibrary[i].read = 'no';
+	} else if (myLibrary[i].read === 'no') {
+		myLibrary[i].read = 'yes';
 	}
 	clearDisplay();
 	renderDisplay();
